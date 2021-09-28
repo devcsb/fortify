e81e9a24127be8bf922b1d794bbb1b636eb107b2
devcsb <devcsb119@gmail.com>
2021-09-28 18:01:29 +0900

    0928 ::18-final

--- database/factories/BoardFactory.php
+++ database/factories/BoardFactory.php
@@ -26,9 +26,10 @@ class BoardFactory extends Factory
         return [
             'name' => User::all()->random()->name,
             // 'email' => User::all()->random()->email,
-            'email' => function ($name) { //전달받은 변수명 값을 어디서 무엇을 받아오는 것인지?
+            'email' => function ($dd) { //전달받은 변수명 값을 어디서 무엇을 받아오는 것인지?
                 // return User::where('name', '=', User::find($board['name']));
-                return User::where('name', $name)->value('email');
+                dd($dd);
+                return User::where('name', $dd)->value('email');
                 // return User::find($board['name']);
             },
             'title' => $this->faker->sentence(),
