14aa70b418fa4e0bbc567dd4a1f3bd2df558b1b7
devcsb <devcsb119@gmail.com>
2021-10-28 23:30:45 +0900

    secret post working on it

--- database/migrations/2021_10_26_142206_create_qnaboards_table.php
+++ database/migrations/2021_10_26_142206_create_qnaboards_table.php
@@ -20,7 +20,7 @@ class CreateQnaboardsTable extends Migration
             $table->string('title');
             $table->text('content');
             $table->unsignedInteger('hits')->default(0);
-            $table->tinyInteger('secret_flag');
+            $table->tinyInteger('secret_flag')->default(0)->comment('공개글:0, 비밀글:1(checkbox checked값)');
             $table->unsignedInteger('group')->nullable()->comment('부모글 번호로 그루핑');
             $table->unsignedInteger('step')->nullable()->comment('계층 정보');
             $table->unsignedInteger('indent')->nullable();
